87ab6c4efd89411f2c0149cfd2b7cf7c9c4160cf
zhupeiwen <1204277355@qq.com>
2021-11-25 16:12:40 +0800

    【UV计算】增量聚合 + 全窗口聚合 + 去重

---
+++ src/main/java/com/tutorial/example/uniquevisitor/Example_01_UV.java
@@ -0,0 +1,92 @@
+package com.tutorial.example.uniquevisitor;
+
+import com.tutorial.bean.Event;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.HashSet;
+
+/**
+ * <p>author     : zhupeiwen
+ * <p>date       : 2021/11/25 2:28 下午
+ * <p>description: UV 独立访客计算(每10S的UV数据) 【增量聚合 + 全窗口聚合的方式】
+ * 这种方式一定程度上肩减少了内存的占用，但用于去重的累加器 HashSet 当用户比较多时，内存有压力
+ */
+public class Example_01_UV {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        env
+                .readTextFile("/Users/jason/MyProjects/FlinkTutorial/src/main/resources/clicks.csv")
+                .map(new MapFunction<String, Event>() {
+                    @Override
+                    public Event map(String value) throws Exception {
+                        String[] arr = value.split(",");
+                        return Event.of(arr[0], arr[1], Long.parseLong(arr[2]) * 1000L);
+                    }
+                })
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(0))
+                                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                                    @Override
+                                    public long extractTimestamp(Event event, long recordTimestamp) {
+                                        return event.timestamp;
+                                    }
+                                })
+                )
+                // ① 因为要开窗口 ② 增量聚合和全窗口聚合结合使用 把其处理为KeyedStream
+                .keyBy(e -> true)
+                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+                .aggregate(new CountAgg(), new WindowResult())
+                .print();
+
+        env.execute();
+    }
+
+    public static class CountAgg implements AggregateFunction<Event, HashSet<String>, Long> {
+
+        @Override
+        public HashSet<String> createAccumulator() {
+            return new HashSet<>();
+        }
+
+        @Override
+        public HashSet<String> add(Event value, HashSet<String> accumulator) {
+            accumulator.add(value.user);
+            return accumulator;
+        }
+
+        @Override
+        public Long getResult(HashSet<String> accumulator) {
+            return (long) accumulator.size();
+        }
+
+        @Override
+        public HashSet<String> merge(HashSet<String> a, HashSet<String> b) {
+            return null;
+        }
+    }
+
+    public static class WindowResult extends ProcessWindowFunction<Long, String, Boolean, TimeWindow> {
+
+        @Override
+        public void process(Boolean aBoolean, Context context, Iterable<Long> elements, Collector<String> out) throws Exception {
+            String start = new Timestamp(context.window().getStart()).toString();
+            String end = new Timestamp(context.window().getEnd()).toString();
+            Long cnt = elements.iterator().next();
+            out.collect(start + " ~ " + end + "的UV数据是：" + cnt);
+        }
+    }
+
+}
